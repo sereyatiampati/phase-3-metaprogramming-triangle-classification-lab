a7f2fb2679bc1593b15419bba661d89112637387
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-02-14 15:37:38 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -34,29 +34,3 @@ class Triangle
   end
 
 end
-
-# class Triangle
-#   # write code here
-#   attr_accessor :length1, :length2, :length3
-  
-#     def initialize(length1, length2, length3)
-#       @length1=length1
-#       @length2=length2
-#       @length3=length3
-#     end
-#     def kind
-#       @sides=Array.new
-#       @sides << length1
-#       @sides << length2
-#       @sides << length3
-#       type=@sides.uniq.length
-      
-#       if type == 1
-#         :equilateral
-#       elsif type == 2
-#         :isosceles
-#       elsif type == 3
-#         :scalene
-#       end
-#     end   
-# end
